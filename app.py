692a83d4bfd19700b6d4f24ff49a4ef5e556629d
ehuarsam <ehuarsaya@unsa.edu.pe>
2026-09-08 16:54:24 -0500

    corregir issues de seguridad detectados por SonarQube (CSRF y debug mode)

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 
+from flask_wtf.csrf import CSRFProtect
+
 app = Flask(__name__)
+csrf = CSRFProtect(app)
+
 DB = "database.db"
 
 
